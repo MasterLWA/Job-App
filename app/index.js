399dc91aa6ff859f59642cdac8cb79a36c28e526
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-07-02 02:17:51 +0530

    just created app

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { SafeAreaView, ScrollView, View, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {Nearbyjobs,Popularjobs,ScreenHeaderBtn, Welcome,} from "../components";
+
+export default function Home() {
+
+  // const router = useRouter(); 
+
+  return (
+    <View>
+      <Text>Hello, P!</Text>
+    </View>
+  );
+}
+
